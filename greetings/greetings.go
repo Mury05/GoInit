e56c05921f3de7b6528e8fbeee06cd90ccfa3880
Mury05 <murielleodilone@gmail.com>
2025-03-25 09:44:31 +0100

    enhance greetings functionality to handle empty names and update hello.go for error logging

--- greetings/greetings.go
+++ greetings/greetings.go
@@ -1,8 +1,16 @@
 package greetings
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+)
 
-func Hello(name string) string {
+// Returns a greeting for the named person
+func Hello(name string) (string, error) {
+	// if no name was given, return an error
+	if name == "" {
+		return "", errors.New("Empty name")
+	}
 	message := fmt.Sprintf("Hello, %v. Welcome", name)
-	return message
+	return message, nil
 }
