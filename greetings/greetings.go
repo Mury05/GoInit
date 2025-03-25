1374dfee36387c8bd7d533d76f5030943f74a695
Mury05 <murielleodilone@gmail.com>
2025-03-25 09:33:32 +0100

    refactor greetings package and update hello.go to utilize greetings functionality(module)

--- greetings/greetings.go
+++ greetings/greetings.go
@@ -1,8 +1,6 @@
 package greetings
 
-import (
-	"fmt"
-)
+import "fmt"
 
 func Hello(name string) string {
 	message := fmt.Sprintf("Hello, %v. Welcome", name)
--- hello/hello.go
+++ hello/hello.go
@@ -2,9 +2,12 @@ package main
 
 import (
 	"fmt"
+
+	"example.com/greetings"
 )
 
 func main() {
 	fmt.Println("Hello, World!")
-
+	message := greetings.Hello("Gladys") // Ensure the greetings package is correctly implemented
+	fmt.Println(message)
 }
